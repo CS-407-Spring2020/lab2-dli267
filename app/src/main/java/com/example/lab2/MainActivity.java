26265e9233b3e58d20acd6cdc53ed9bf81c50b47
dli267@wisc.edu <Quartermoon2>
2020-02-10 14:59:55 -0600

    added new activity, button

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -2,9 +2,23 @@ package com.example.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.View;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view) {
+
+        Log.i("Info", "Button pressed");
+
+        EditText myTextField = (EditText) findViewById(R.id.myTextField);
+
+        Toast.makeText(MainActivity.this, myTextField.getText().toString(), Toast.LENGTH_LONG).show();
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
